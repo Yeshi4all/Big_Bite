500696a880de9da01fe82abe0915049107867d37
reshmaresh94 <reshmaresh94@gmail.com>
2023-08-14 13:47:01 -0400

    Dynamic images in the Customer pages

--- server/controllers/cust_menuController.js
+++ server/controllers/cust_menuController.js
@@ -1,6 +1,6 @@
 const Menu = require('../models/Menu');
 const mongoose = require('mongoose');
-
+const mime = require('mime-types');
 
 /*
  * GET
@@ -10,6 +10,15 @@ const mongoose = require('mongoose');
 exports.cust_menupage = async (req,res) => {
     try{
         const menus = await Menu.find({});
+
+        // Iterate through menus and convert the Buffer to a base64 string
+        menus.forEach((menu) => {
+            menu.imageData = menu.imageData ? menu.imageData : null;
+            // Set the image extension if known (e.g., 'png', 'jpg', etc.)
+            menu.imageExtension = menu.imageData ? mime.extension(menu.imageMimeType) : null;
+          });
+
+          
         res.render('dashboard/bigbitemenu', {
             menus,
             layout: false });
